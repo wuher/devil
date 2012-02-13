906e224dc51790646b745132d403f91ad9885e35
wuher <janne.kuuskeri@gmail.com>
2012-02-13 23:17:43 +0200

    added dependencies to the setup script

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from distutils.core import setup
 
 setup(
     name='devil',
-    version='0.2',
+    version='0.3',
     author='Janne Kuuskeri (wuher)',
     author_email='janne.kuuskeri@gmail.com',
     url='https://github.com/wuher/devil/',
-    packages=['devil',],
+    packages=['devil', 'devil.perm', 'devil.mappers'],
+    install_requires=['simplejson>=2.1.0', 'django>=1.2.0'],
     license='MIT',
     description='Simple REST framework for Django',
     long_description=open('README.markdown').read(),
