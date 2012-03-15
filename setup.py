215b51b5330474422778e0c4cecc53847cd93cb4
wuher <janne.kuuskeri@gmail.com>
2012-03-15 15:13:12 +0200

    bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='devil',
-    version='0.4',
+    version='0.5',
     author='Janne Kuuskeri (wuher)',
     author_email='janne.kuuskeri@gmail.com',
     url='https://github.com/wuher/devil/',
