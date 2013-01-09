8768b190aa89f621f348553a6c56f84dcc770cac
Janne Kuuskeri <janne.kuuskeri@gmail.com>
2013-01-10 00:16:21 +0200

    added missing package dependency into setup script

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Janne Kuuskeri (wuher)',
     author_email='janne.kuuskeri@gmail.com',
     url='https://github.com/wuher/devil/',
-    packages=['devil', 'devil.perm', 'devil.mappers'],
+    packages=['devil', 'devil.perm', 'devil.mappers', 'devil.fields'],
     install_requires=['simplejson>=2.1.0', 'django>=1.3.0'],
     license='MIT',
     description='Simple REST framework for Django',
